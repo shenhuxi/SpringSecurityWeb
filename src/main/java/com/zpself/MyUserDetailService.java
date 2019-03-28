be329293b92417ab7b6961a9747c51b7bd8086bc
shenhuxi <1769411197@qq.com>
2019-03-28 17:19:47 +0800

    提交基础代码

---
+++ src/main/java/com/zpself/MyUserDetailService.java
@@ -0,0 +1,22 @@
+package src.main.java.com.zpself;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Created by zengpeng on 2019/3/28
+ */
+public class MyUserDetailService implements UserDetailsService {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new User(username,passwordEncoder.encode("123456"),
+                AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+    }
+}
---
+++ src/main/java/com/zpself/SpringSecurityApplication.java
@@ -0,0 +1,21 @@
+package src.main.java.com.zpself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zengpeng on 2019/3/28
+ */
+@SpringBootApplication
+@RestController
+public class SpringSecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApplication.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
